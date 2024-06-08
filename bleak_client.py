633a1805b02fee6e4c8f58d85dc756dd17f894c1
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2024-06-08 18:20:05 +0300

    Fix queuing and add colored logging

--- bleak_client.py
+++ bleak_client.py
@@ -1,14 +1,13 @@
 import asyncio
-import logging
 import concurrent.futures
-from queue import Queue
-from typing import Optional
-import time
 import datetime
 import json
+import logging
+import time
+from typing import Optional
 
+import colorlog
 import msgpack
-
 from bleak import BleakClient, BleakScanner, BLEDevice
 from bleak.backends.characteristic import BleakGATTCharacteristic
 
@@ -16,9 +15,9 @@ SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 DEVICES = [
-    # "08:D1:F9:C7:14:DE",  # ESP32 DevkitC v4 1 (Left arm)
-    # "08:D1:F9:DF:D7:BA",  # ESP32 DevkitC v4 2 (Right arm)
-    # "CD:C8:D6:CF:45:50",  # XIAO 1 (Left leg)
+    "08:D1:F9:C7:14:DE",  # ESP32 DevkitC v4 1 (Left arm)
+    "08:D1:F9:DF:D7:BA",  # ESP32 DevkitC v4 2 (Right arm)
+    "CD:C8:D6:CF:45:50",  # XIAO 1 (Left leg)
     "D9:4D:33:22:7F:55",  # XIAO 2 (Right leg)
 ]
 
@@ -29,11 +28,64 @@ DEVICE_NAMES = [
     "RIGHT_LEG",
 ]
 
+
+class ListQueue:
+    def __init__(self, max_size=20):
+        self.queue = []
+        self.max_size = max_size
+
+    def put(self, item):
+        self.queue.append(item)
+        if len(self.queue) > self.max_size:
+            self.queue.pop(0)
+
+    def get(self):
+        return self.queue.pop(0)
+
+    def get_front(self):
+        return self.queue.pop(-1)
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def qsize(self):
+        return len(self.queue)
+
+    def clear(self):
+        self.queue.clear()
+
+    def truncate(self, size):
+        self.queue = self.queue[-size:]
+
+
+MAX_QUEUE_SIZE = 20
+TRUNCATED_QUEUE_SIZE = 5
+
 bleak_clients: list[Optional[BleakClient]] = [None] * len(DEVICES)
 
-notification_queues = [Queue() for _ in DEVICES]
+notification_queues = [ListQueue(MAX_QUEUE_SIZE) for _ in DEVICES]
 
-logger = logging.getLogger(__name__)
+# Setup logging
+LOG_FORMAT = "%(log_color)s%(asctime)-15s %(name)-8s %(levelname)s: %(message)s"
+# LOG_FORMAT = "%(log_color)s%(levelname)s:%(name)s:%(message)s"
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(
+    colorlog.ColoredFormatter(
+        LOG_FORMAT,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+)
+
+logger = colorlog.getLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
 
@@ -42,7 +94,9 @@ def handle_notification(
     index: int, characteristic: BleakGATTCharacteristic, data: bytearray
 ):
     notification_queues[index].put((time.time(), data))
+
     # logger.info(f"Notified by {DEVICE_NAMES[index]}: {data.hex()}")
+    # logger.info(f"Notified by {DEVICE_NAMES[index]}. Queue size: {notification_queues[index].qsize()}")
 
 
 def thread_callback(callback, *args):
@@ -52,12 +106,11 @@ def thread_callback(callback, *args):
 
 
 def disconnect_client(index: int, client: BleakClient):
-    logger.info(f"Disconnected from {DEVICE_NAMES[index]}")
+    logger.error(f"Disconnected from {DEVICE_NAMES[index]}")
     bleak_clients[index] = None
 
 
 CONNECTION_TIMEOUT = 8
-
 
 
 async def add_client(index: int, device: BLEDevice):
@@ -98,6 +151,7 @@ async def add_client(index: int, device: BLEDevice):
 
 
 # Thresholds in seconds
+MAIN_LOOP_INTERVAL = 1
 MAX_NOTIFICATION_INTERVAL = 0.150
 DATA_VALIDITY_THRESHOLD = 0.800
 SCAN_TIMEOUT = 1.5
@@ -149,7 +203,7 @@ async def check_and_reconnect():
 def combine_data_and_send() -> Optional[dict]:
     while True:
         # Get the latest notification from each queue
-        latest_notifications = [
+        latest_notifications: list[Optional[tuple[float, bytearray]]] = [
             q.queue[-1] if not q.empty() else None for q in notification_queues
         ]
 
@@ -173,19 +227,23 @@ def combine_data_and_send() -> Optional[dict]:
                 for i, n in enumerate(latest_notifications):
                     last_i = i
                     combined_data[DEVICE_NAMES[i]] = msgpack.unpackb(n[1])
+                    # combined_data[DEVICE_NAMES[i]] = json.loads(n[1].decode())
 
                 # Send combined data to server Pi
-                logger.info(f"Combined data: {combined_data}")
+                logger.info(f"Combined data: {combined_data}\n\n")
 
                 # combined_data_packed = msgpack.packb(combined_data)
 
-                # Remove the notifications from the queues
+                # Remove older notifications from the queues
                 for q in notification_queues:
-                    q.get()
+                    if not q.empty():
+                        q.truncate(TRUNCATED_QUEUE_SIZE)
 
                 return combined_data
             except Exception as e:
                 logger.error(f"Error unpacking data from {DEVICE_NAMES[last_i]}: {e}")
+                print(f"Received data:\n{latest_notifications[last_i][1]}")
+                # print(f'Received data:\n{latest_notifications[last_i][1].decode()}')
                 return
 
         # Find the queue with the latest data
@@ -194,14 +252,21 @@ def combine_data_and_send() -> Optional[dict]:
         ]
 
         # If the current time minus the max is larger than a the data validity threshold, break the loop
-        if time.time() - max_queue.queue[-1][0] > DATA_VALIDITY_THRESHOLD:
+        time_diff = time.time() - max_queue.queue[-1][0]
+        if time_diff > DATA_VALIDITY_THRESHOLD:
             logger.warning(
-                f"Skipping combined packet due to large time difference ({time_diff}s)"
+                f"Skipping combined packet due to data being too old ({int(time_diff * 1000)}ms)"
             )
+
+            # Empty all of the queues since all data is old now
+            for q in notification_queues:
+                if not q.empty():
+                    q.clear()
+
             return
 
         # Remove the latest notification from the queue with the latest data
-        max_queue.get()
+        max_queue.get_front()
 
 
 def save_file():
@@ -210,7 +275,7 @@ def save_file():
     with open(fname, "w") as f:
         json.dump({"data": data}, f)
 
-    print(f"\n************** Saved {fname} **************\n")
+    logger.info(f"************** Saved {fname} **************\n")
 
 
 async def main():
@@ -223,32 +288,33 @@ async def main():
         data.append(combined_data)
         count += 1
 
-        # Save every 5 items or 10 seconds
+        # Save every 5 items or 10 iterations
         if len(data) >= 5 or (count >= 10 and len(data) > 0):
             count = 0
             save_file()
             data.clear()
 
-
         # The script will crash on Linux if we create two instances of BleakScanner
         if not is_scanning:
             await check_and_reconnect()
 
-        await asyncio.sleep(1)  # Sleep for a while before checking again
+        # Sleep for a while before checking again
+        await asyncio.sleep(MAIN_LOOP_INTERVAL)  
+
+
+def disconnect_all():
+    for i, client in enumerate(bleak_clients):
+        if client is not None:
+            disconnect_client(i, client)
 
 
 if __name__ == "__main__":
-    log_level = logging.INFO
-    logging.basicConfig(
-        level=log_level,
-        format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
-    )
-
     try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        disconnect_all()
+        logger.info("Disconnected all devices")
     except BaseException as e:
-        for i, client in enumerate(bleak_clients):
-            if client is not None:
-                disconnect_client(i, client)
+        disconnect_all()
 
         raise e
